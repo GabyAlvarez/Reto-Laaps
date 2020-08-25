625ccf4ed8e0eb58899f891210eee916ffe436a4
Dianyela Maldonado <dianyela.y.maldonado@gmail.com>
2020-08-25 13:23:10 -0500

    add https of package json

--- src/components/Geolocation/geolocation.js
+++ src/components/Geolocation/geolocation.js
@@ -11,8 +11,7 @@ function Map() {
     return (
         <GoogleMap
             defaultZoom={15}
-            defaultCenter={{ lat: 45.4211, lng: -75.6903 }}
-        // defaultCenter={{ lat: -99.2288, lng: 18.4816 }}
+            defaultCenter={{ lat: -99.2288, lng: 18.4816 }}
         >
         </GoogleMap>
     );
@@ -26,7 +25,7 @@ function Geolocation() {
     return (
         <div style={{ width: "100vw", height: "100vh" }}>
             <MapWrapped
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdpjyXtAodo62WoyiJWFqezrGsCA0dDQk&callback=initMap"
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtKEa9SMUGwnEYzP0aFtEnYfAZL6Mve_8&callback=initMap"
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `100%` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
